ebecd19ccf554640df8db8ced2b7a5a6fe1b904f
Heriklys Andrade <heriklys.andrade@totvs.com.br>
2021-05-19 17:20:31 -0300

    Add project files.

---
+++ BasicPdfWebApp/Models/FileContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicPdfWebApp.Models
+{
+    public class FileContext : DbContext
+    {
+        public FileContext(DbContextOptions<FileContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<FileItem> FileItems{ get; set; }
+    }
+}
---
+++ BasicPdfWebApp/Models/FileItemDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicPdfWebApp.Models
+{
+    public class FileItemDTO
+    {
+        public int Id { get; set; }
+
+        //public Guid Guid { get; set; }
+
+        public string FileFullPath { get; set; }
+    }
+}
